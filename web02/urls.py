438584bd45f9716c4d0772c709172d29ed8fa623
hengloem <heng.loem2017@gmail.com>
2019-09-23 15:34:48 +0700

    URLs and views

--- web02/urls.py
+++ web02/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^post/$', blog_views.post),
+    url(r'^$', blog_views.index),
 ]
