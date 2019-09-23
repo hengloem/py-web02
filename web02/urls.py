49f1fc5767b60c0298d33f7e936cfb70c06404b4
hengloem <heng.loem2017@gmail.com>
2019-09-23 16:51:40 +0700

    Dynamic template data

--- web02/urls.py
+++ web02/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, re_path
 from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^post/$', blog_views.post),
-    url(r'^$', blog_views.index),
+    re_path(r'^$', blog_views.index),
+    re_path(r'^post/$', blog_views.post),
 ]
