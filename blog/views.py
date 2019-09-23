b8a3d79c063daa25073763a119bf5b217a7c01d0
hengloem <heng.loem2017@gmail.com>
2019-09-23 15:50:27 +0700

    HTML templates

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hey, Heng")
+    return render(request,"index.html", {})
 
 
 def post(request):
